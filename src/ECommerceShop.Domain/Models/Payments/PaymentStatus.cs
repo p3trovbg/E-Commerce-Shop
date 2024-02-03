092cabc84c7e5e4142635ddddd14370926e8486a
Georgi Petrov <p3trovsit@gmail.com>
2024-02-03 18:33:54 +0200

    add payment status and type in payment context

---
+++ src/ECommerceShop.Domain/Models/Payments/PaymentStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace ECommerceShop.Domain.Models.Payments;
+
+using ECommerceShop.Domain.Common;
+
+public class PaymentStatus : Enumeration
+{
+    public static readonly PaymentStatus NotConfirmed = new PaymentStatus(1, nameof(NotConfirmed));
+    public static readonly PaymentStatus Pending = new PaymentStatus(2, nameof(Pending));
+    public static readonly PaymentStatus Confirmed = new PaymentStatus(3, nameof(Confirmed));
+    public static readonly PaymentStatus Success = new PaymentStatus(4, nameof(Success));
+    public static readonly PaymentStatus Rejected = new PaymentStatus(5, nameof(Rejected));
+    public static readonly PaymentStatus Expired = new PaymentStatus(6, nameof(Expired));
+    public static readonly PaymentStatus Refunded = new PaymentStatus(7, nameof(Refunded));
+
+    private PaymentStatus(int value) : this(value, FromValue<PaymentType>(value).Name)
+    {
+    }
+
+    private PaymentStatus(int value, string name) : base(value, name)
+    {
+    }
+}
---
+++ src/ECommerceShop.Domain/Models/Payments/PaymentType.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceShop.Domain.Models.Payments;
+
+using Common;
+
+public class PaymentType : Enumeration
+{
+    public static readonly PaymentType Cash = new PaymentType(1, nameof(Cash));
+    public static readonly PaymentType BankWire = new PaymentType(2, nameof(BankWire));
+    public static readonly PaymentType OnlineWithCreditCard = new PaymentType(3, nameof(OnlineWithCreditCard));
+
+    private PaymentType(int value) : this(value, FromValue<PaymentStatus>(value).Name)
+    {
+    }
+
+    private PaymentType(int value, string name) : base(value, name)
+    {
+    }
+}
