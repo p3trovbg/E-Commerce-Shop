ff6ff729f9b837d3bc2e1b5cf3898703d8e44685
Georgi Petrov <p3trovsit@gmail.com>
2024-02-03 18:32:33 +0200

    fix namespaces and unnecessary staffs

--- src/ECommerceShop.Domain/DomainConfiguration.cs
+++ src/ECommerceShop.Domain/DomainConfiguration.cs
@@ -1,18 +1,17 @@
-﻿namespace ECommerceShop.Domain
+﻿namespace ECommerceShop.Domain;
+
+using Factories;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DomainConfiguration
 {
-    using Factories;
-
-    using Microsoft.Extensions.DependencyInjection;
-
-    public static class DomainConfiguration
-    {
-        public static IServiceCollection AddDomain(this IServiceCollection services)
-            => services
-                .Scan(s => s
-                    .FromCallingAssembly()
-                    .AddClasses(c => c
-                        .AssignableTo(typeof(IFactory<>)))
-                .AsMatchingInterface()
-                .WithTransientLifetime());
-    }
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+        => services
+            .Scan(s => s
+                .FromCallingAssembly()
+                .AddClasses(c => c
+                    .AssignableTo(typeof(IFactory<>)))
+            .AsMatchingInterface()
+            .WithTransientLifetime());
 }
