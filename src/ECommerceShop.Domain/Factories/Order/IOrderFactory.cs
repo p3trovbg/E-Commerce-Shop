a614ed0b493ed3c4e02e210d30162d5f9558279a
Georgi Petrov <p3trovsit@gmail.com>
2024-02-03 18:33:40 +0200

    adding order factory

---
+++ src/ECommerceShop.Domain/Factories/Order/IOrderFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceShop.Domain.Factories.Order;
+
+using Models.Orders;
+
+public interface IOrderFactory : IFactory<Order>
+{
+    IOrderFactory WithAddress(string address);
+
+    IOrderFactory WithTotalPrice(decimal totalPrice);
+
+    IOrderFactory WithTotalDiscount(decimal totalDiscount);
+
+    IOrderFactory WithOrderStatus(OrderStatus status);
+
+    IOrderFactory WithRecipient(string name, string phoneNumber, string email);
+
+    IOrderFactory WithRecipient(Recipient recipient);
+}
---
+++ src/ECommerceShop.Domain/Factories/Order/OrderFactory.cs
@@ -0,0 +1,68 @@
+﻿namespace ECommerceShop.Domain.Factories.Order;
+
+using Exceptions;
+using Models.Orders;
+
+internal class OrderFactory : IOrderFactory
+{
+    private string address = default!;
+    private decimal totalPrice = default!;
+    private decimal totalDiscount = default!;
+    private OrderStatus status = default!;
+    private Recipient recipient = default!;
+
+    private bool recipientSet = false;
+
+    public IOrderFactory WithAddress(string address)
+    {
+        this.address = address;
+        return this;
+    }
+
+    public IOrderFactory WithTotalPrice(decimal totalPrice) 
+    {
+        this.totalPrice = totalPrice;
+        return this;
+    }
+
+    public IOrderFactory WithTotalDiscount(decimal totalDiscount)
+    {
+        this.totalDiscount = totalDiscount;
+        return this;
+    }
+
+    public IOrderFactory WithOrderStatus (OrderStatus status)
+    {
+        this.status = status;
+        return this;
+    }
+
+    public IOrderFactory WithRecipient(string name, string phoneNumber, string email)
+        => WithRecipient(new Recipient(name, phoneNumber, email));
+
+    public IOrderFactory WithRecipient(Recipient recipient)
+    {
+        this.recipientSet = true;
+        this.recipient = recipient;
+        return this;
+    }
+
+    public Order Build()
+    {
+        ValidateSets();
+        return new Order(
+            this.address,
+            this.totalPrice,
+            this.totalDiscount,
+            this.status,
+            this.recipient);
+    }
+
+    private void ValidateSets()
+    {
+        if (!this.recipientSet)
+        {
+            throw new InvalidOrderException();
+        }
+    }
+}
