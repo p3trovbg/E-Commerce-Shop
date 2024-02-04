b7182f41897bfb293584c27f6e93fbbafc80d035
Georgi Petrov <p3trovsit@gmail.com>
2024-02-04 16:51:58 +0200

    add shopping cart context

---
+++ src/ECommerceShop.Domain/Models/ShoppingCarts/Product.cs
@@ -0,0 +1,63 @@
+﻿namespace ECommerceShop.Domain.Models.ShoppingCarts;
+
+using Common;
+using Exceptions;
+
+using static ModelConstants.Product;
+using static ModelConstants.Money;
+
+public class Product : Entity<int>
+{
+    internal Product(string name, decimal price, int quantity, string imagePath, bool isAvaliable) 
+    {
+        Validate(name, price, quantity, imagePath);
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
+        this.ImagePath = imagePath;
+        this.IsAvaliable = isAvaliable;
+    }
+
+    public string Name { get; private set; }
+
+    public decimal Price { get; private set; }
+
+    public int Quantity { get; private set; }
+
+    public string ImagePath { get; private set; }
+
+    public bool IsAvaliable { get; private set; }
+
+
+    private void Validate(string name, decimal price, int quantity, string imagePath)
+    {
+        ValidateName(name);
+        ValidatePrice(price);
+        ValidateQuantity(quantity);
+        ValidateImagePath(imagePath);
+    }
+
+    private void ValidateName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ValidatePrice(decimal price)
+        => Guard.AgainstOutOfRange<InvalidProductException>(
+            price,
+            MinAmount,
+            MaxAmount,
+            nameof(this.Price));
+
+    private void ValidateQuantity(int quantity)
+        => Guard.AgainstOutOfRange<InvalidProductException>(
+            quantity,
+            MinQuantity,
+            MaxQuantity,
+            nameof(this.Quantity));
+
+    private void ValidateImagePath(string imagePath)
+        => Guard.AgainstEmptyString<InvalidImageException>(
+            imagePath,
+            nameof(this.ImagePath));
+}
---
+++ src/ECommerceShop.Domain/Models/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿namespace ECommerceShop.Domain.Models.ShoppingCarts;
+
+using Common;
+using Exceptions;
+
+using static ModelConstants.Money;
+
+public class ShoppingCart : Entity<int>, IAggregateRoot
+{
+    private readonly HashSet<Product> products;
+
+    internal ShoppingCart(decimal totalPrice)
+    {
+        Validate(totalPrice);
+        this.TotalPrice = totalPrice;
+        this.products = new HashSet<Product>();
+    }
+
+    public decimal TotalPrice { get; private set; }
+
+    public IReadOnlyCollection<Product> Products => this.products.ToList().AsReadOnly();
+
+    public void AddProduct(Product product) => this.products.Add(product);
+
+    public ShoppingCart UpdateTotalPrice(decimal totalPrice)
+    {
+        ValidateTotalPrice(totalPrice);
+        this.TotalPrice = totalPrice;
+        return this;
+    }
+
+    private void Validate(decimal totalPrice)
+    {
+        ValidateTotalPrice(totalPrice);
+    }
+
+    private void ValidateTotalPrice(decimal totalPrice)
+        => Guard.AgainstOutOfRange<InvalidMoneyException>(
+            totalPrice,
+            MinAmount,
+            MaxAmount,
+            nameof(this.TotalPrice));
+}
